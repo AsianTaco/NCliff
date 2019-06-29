6852074612cd833f3cde0862dbfbecc63f23be6b
simon <blabla@bla>
2019-06-29 23:47:58 +0200

    Improved visualization of action values and training result; Added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cliff Walking problem
+
+An implementation of tabular Q-learning for the Cliff Walking problem
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import Gameboard
-import Agent
+from Gameboard import Gameboard
+from Agent import Agent
 
-agent = Agent.Agent()
+import numpy as np
+import matplotlib.pyplot as plt
 
+
+agent = Agent()
+
+game_record = []
+
+print('Training agent for {} games'.format(100))
 for i in range(100):
-    game = Gameboard.Gameboard()
+    game = Gameboard()
 
     while game.getGameStatus() == 0:
         firstPosition = game.getPosition()
@@ -14,7 +21,16 @@ for i in range(100):
         reward = agent.getReward(game)
         agent.updateTable(firstPosition, newPosition, actionIndex, reward)
 
-game = Gameboard.Gameboard()
+    game_record.append(game.getGameStatus())
+
+plt.plot(game_record)
+plt.ylabel('Game results')
+plt.title('Game record over {} games'.format(100))
+plt.show()
+
+print('Testing trained agent for one game')
+
+game = Gameboard()
 game.showBoard()
 
 while game.getGameStatus() == 0:
@@ -22,4 +38,11 @@ while game.getGameStatus() == 0:
     agent.execAction(game, actionIndex)
     game.showBoard()
 
-print(agent.q_valueTable)
+print('Action values for moving up at this position:')
+print(np.matrix(np.around(agent.q_valueTable[:,:,0],2)))
+print('Action values for moving down at this position:')
+print(np.matrix(np.around(agent.q_valueTable[:,:,1],2)))
+print('Action values for moving left at this position:')
+print(np.matrix(np.around(agent.q_valueTable[:,:,2],2)))
+print('Action values for moving right at this position:')
+print(np.matrix(np.around(agent.q_valueTable[:,:,3],2)))
